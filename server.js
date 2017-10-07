865549f119a666ab713a67a1529c14d660da2503
HOME\Garry <gtoborn@hotmail.com>
2017-10-06 20:13:22 -0600

    first commit

--- server.js
+++ server.js
@@ -1,5 +1,28 @@
 // TODO: create a basic server with express
 // that will send back the index.html file on a GET request to '/'
 // it should then send back jsonData on a GET to /data
+var express = require('express');
+var app = express();
 
 var jsonData = {count: 12, message: 'hey'};
+
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/index.html', function(err) {
+        if (err) {
+            res.status(500).send(err);
+        }
+    })
+});
+
+// on get request to url
+app.get('/data', function(req, res) {
+    res.json(jsonData);
+});
+
+
+//start server on port 3000
+var port = 3000;
+app.listen(port, function() {
+    console.log('Listening on port', port );
+});
+
